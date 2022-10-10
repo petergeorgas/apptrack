8a45e89ce9071c9d44a13ece254ba135432b3d01
Peter Georgas <peter_georgas@yahoo.com>
2022-10-10 17:18:02 -0400

    Add updated favicon, page name to site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import client from "../apollo/client";
 import "../styles/globals.css";
 import theme from "../theme/theme";
@@ -9,6 +10,31 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ApolloProvider client={client}>
 			<ChakraProvider>
+				<Head>
+					<title>apptrack</title>
+
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+					<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+					<meta name="msapplication-TileColor" content="#da532c" />
+					<meta name="theme-color" content="#ffffff" />
+				</Head>
 				<ColorModeScript initialColorMode={theme.config.initialColorModes} />
 				<Component {...pageProps} />
 			</ChakraProvider>
